3969b40b1a10d8eed6831aad78933de722f28d7c
Axhar85 <azhararshad_ntu@yahoo.com>
2021-12-14 09:30:36 +0100

    passing password elemets to signup form

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -20,16 +20,40 @@ const FormSignup = () => {
                         placeholder='Enter your name'/>
                 </div>
                 <div className='form-inputs'>
-                    <label htmlFor='e-mail' 
+                    <label htmlFor='email' 
                     className='form-lable'>
                         e-mail
                     </label>
                     <input
                         id='e-mail'
-                        type='text' 
-                        name='username' 
+                        type='email' 
+                        name='email' 
                         className='form-input' 
-                        placeholder='Enter your name'/>
+                        placeholder='Enter your email'/>
+                </div>
+                <div className='form-inputs'>
+                    <label htmlFor='password' 
+                    className='form-lable'>
+                        Password
+                    </label>
+                    <input
+                        id='password'
+                        type='password' 
+                        name='password' 
+                        className='form-input' 
+                        placeholder='Enter your password'/>
+                </div>
+                <div className='form-inputs'>
+                    <label htmlFor='password2' 
+                    className='form-lable'>
+                        Password2
+                    </label>
+                    <input
+                        id='password2'
+                        type='password2' 
+                        name='password2' 
+                        className='form-input' 
+                        placeholder='Enter your password2'/>
                 </div>
             </form>
         </div>
