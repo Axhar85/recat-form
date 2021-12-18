408e6458b5f37bd13e7595c3fc2355783826d948
Axhar85 <azhararshad_ntu@yahoo.com>
2021-12-18 08:33:22 +0100

    adding images

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const FormSuccess = () => {
     return (
-        <div>
-            
+        <div className='form-content-right'>
+            <div className='form-success'>
+                We have recieved your request
+            </div>
+            <img src='img/img-3.svg' alt='success-image'
+            className='form-image-2'/>
         </div>
     )
 }
